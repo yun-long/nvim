1b511f2726ec2c8baa0ec91fcdb43dbd07f5e99e
Yunlong Song <yun-long.song@outlook.com>
2025-02-28 11:59:54 -0500

    wip

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,153 +1,224 @@
 return {
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
-    config = function()
-      require("avante").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-        provider = "claude", -- Recommend using Claude
-        -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
-        -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
-        -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
-        auto_suggestions_provider = "claude",
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-3-5-sonnet-20241022",
-          temperature = 0,
-          max_tokens = 4096,
-        },
-        behaviour = {
-          auto_suggestions = false, -- Experimental stage
-          auto_set_highlight_group = true,
-          auto_set_keymaps = true,
-          auto_apply_diff_after_generation = false,
-          support_paste_from_clipboard = false,
-          minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
-        },
-        mappings = {
-          --- @class AvanteConflictMappings
-          diff = {
-            ours = "co",
-            theirs = "ct",
-            all_theirs = "ca",
-            both = "cb",
-            cursor = "cc",
-            next = "]x",
-            prev = "[x",
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    -- add any opts here
+    -- for example
+     provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+    -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+    -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+    -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+    auto_suggestions_provider = "claude",
+    cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      -- model = "claude-3-7-sonnet-20250219",
+      model = "claude-3-5-haiku-20241022",
+      temperature = 0,
+      max_tokens = 4096,
+      disable_tools = true,
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
           },
-          suggestion = {
-            accept = "<M-l>",
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-          jump = {
-            next = "]]",
-            prev = "[[",
-          },
-          submit = {
-            normal = "<CR>",
-            insert = "<C-s>",
-          },
-          sidebar = {
-            apply_all = "A",
-            apply_cursor = "a",
-            switch_windows = "<Tab>",
-            reverse_switch_windows = "<S-Tab>",
-          },
-        },
-        hints = { enabled = true },
-        windows = {
-          ---@type "right" | "left" | "top" | "bottom"
-          position = "right",
-          wrap = true,
-          width = 30,
-          -- height = 30,
-          sidebar_header = {
-            enabled = true,
-            align = "center",
-            rounded = true,
-          },
-          ask = {
-            floating = false, -- Open the 'AvanteAsk' prompt in a floating window
-            start_insert = true, -- Start insert mode when opening the ask window
-            border = "rounded",
-            ---@type "ours" | "theirs"
-            focus_on_apply = "ours", -- which diff to focus after applying
-          },
-        },
-        mappings = {
-          suggestion = {
-            next = "<Tab>", -- Use Tab for moving to the next suggestion
-            prev = "<S-Tab>", -- Use Shift-Tab for the previous suggestion
-          },
-        },
-        highlights = {
-          ---@type AvanteConflictHighlights
-          diff = {
-            current = "DiffText",
-            incoming = "DiffAdd",
-          },
-        },
-        --- @class AvanteConflictUserConfig
-        diff = {
-          autojump = true,
-          ---@type string | fun(): any
-          list_opener = "copen",
-          --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
-          --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
-          --- Disable by setting to -1.
-          override_timeoutlen = 500,
-        },
-        suggestion = {
-          debounce = 100,
-          throttle = 100,
-        },
-      })
-    end,
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
+          -- required for Windows users
+          use_absolute_path = true,
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
       },
+      ft = { "markdown", "Avante" },
     },
   },
 }
+
+-- return {
+--   {
+--     "yetone/avante.nvim",
+--     event = "VeryLazy",
+--     lazy = false,
+--     version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+--     config = function()
+--       require("avante").setup({
+--         -- your configuration comes here
+--         -- or leave it empty to use the default settings
+--         ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+--         provider = "claude", -- Recommend using Claude
+--         -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+--         -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+--         -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+--         auto_suggestions_provider = "claude",
+--         claude = {
+--           endpoint = "https://api.anthropic.com",
+--           model = "claude-3-7-sonnet-20250219",
+--           temperature = 0,
+--           max_tokens = 4096,
+--         },
+--         behaviour = {
+--           auto_suggestions = false, -- Experimental stage
+--           auto_set_highlight_group = true,
+--           auto_set_keymaps = true,
+--           auto_apply_diff_after_generation = false,
+--           support_paste_from_clipboard = false,
+--           minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+--         },
+--         mappings = {
+--           --- @class AvanteConflictMappings
+--           diff = {
+--             ours = "co",
+--             theirs = "ct",
+--             all_theirs = "ca",
+--             both = "cb",
+--             cursor = "cc",
+--             next = "]x",
+--             prev = "[x",
+--           },
+--           suggestion = {
+--             accept = "<M-l>",
+--             next = "<M-]>",
+--             prev = "<M-[>",
+--             dismiss = "<C-]>",
+--           },
+--           jump = {
+--             next = "]]",
+--             prev = "[[",
+--           },
+--           submit = {
+--             normal = "<CR>",
+--             insert = "<C-s>",
+--           },
+--           sidebar = {
+--             apply_all = "A",
+--             apply_cursor = "a",
+--             switch_windows = "<Tab>",
+--             reverse_switch_windows = "<S-Tab>",
+--           },
+--         },
+--         hints = { enabled = true },
+--         windows = {
+--           ---@type "right" | "left" | "top" | "bottom"
+--           position = "right",
+--           wrap = true,
+--           width = 30,
+--           -- height = 30,
+--           sidebar_header = {
+--             enabled = true,
+--             align = "center",
+--             rounded = true,
+--           },
+--           ask = {
+--             floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+--             start_insert = true, -- Start insert mode when opening the ask window
+--             border = "rounded",
+--             ---@type "ours" | "theirs"
+--             focus_on_apply = "ours", -- which diff to focus after applying
+--           },
+--         },
+--         mappings = {
+--           suggestion = {
+--             next = "<Tab>", -- Use Tab for moving to the next suggestion
+--             prev = "<S-Tab>", -- Use Shift-Tab for the previous suggestion
+--           },
+--         },
+--         highlights = {
+--           ---@type AvanteConflictHighlights
+--           diff = {
+--             current = "DiffText",
+--             incoming = "DiffAdd",
+--           },
+--           conflict = {
+--             current = { fg = "#ffffff", bg = "#2f628e" },   -- Ours/current changes
+--             incoming = { fg = "#ffffff", bg = "#347758" },  -- Theirs/incoming changes
+--             ancestor = { fg = "#ffffff", bg = "#754a87" },  -- Base/ancestor version
+--           },
+--         },
+--         --- @class AvanteConflictUserConfig
+--         diff = {
+--           autojump = true,
+--           ---@type string | fun(): any
+--           list_opener = "copen",
+--           --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+--           --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+--           --- Disable by setting to -1.
+--           override_timeoutlen = 500,
+--         },
+--         suggestion = {
+--           debounce = 100,
+--           throttle = 100,
+--         },
+--       })
+--     end,
+--     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--     build = "make",
+--     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+--     dependencies = {
+--       "stevearc/dressing.nvim",
+--       "nvim-lua/plenary.nvim",
+--       "MunifTanjim/nui.nvim",
+--       --- The below dependencies are optional,
+--       "echasnovski/mini.pick", -- for file_selector provider mini.pick
+--       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--       "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--       "zbirenbaum/copilot.lua", -- for providers='copilot'
+--       {
+--         -- support for image pasting
+--         "HakonHarnes/img-clip.nvim",
+--         event = "VeryLazy",
+--         opts = {
+--           -- recommended settings
+--           default = {
+--             embed_image_as_base64 = false,
+--             prompt_for_file_name = false,
+--             drag_and_drop = {
+--               insert_mode = true,
+--             },
+--             -- required for Windows users
+--             use_absolute_path = true,
+--           },
+--         },
+--       },
+--       {
+--         -- Make sure to set this up properly if you have lazy=true
+--         'MeanderingProgrammer/render-markdown.nvim',
+--         opts = {
+--           file_types = { "markdown", "Avante" },
+--         },
+--         ft = { "markdown", "Avante" },
+--       },
+--     },
+--   },
+-- }
