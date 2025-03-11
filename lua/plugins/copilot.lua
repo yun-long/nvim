2a90eae2d955d41e6d12ddd2713b181cd8aa5d24
Yunlong Song <yun-long.song@outlook.com>
2025-03-11 16:58:31 -0400

    wip

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -37,8 +37,8 @@ return {
           },
         },
         filetypes = {
-          yaml = false,
-          markdown = false,
+          yaml = true,
+          markdown = true,
           help = false,
           gitcommit = false,
           gitrebase = false,
