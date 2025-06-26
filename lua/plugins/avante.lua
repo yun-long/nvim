17c9e10dd440517ad7c7f0c24340fd617db201f6
Yunlong Song <yun-long.song@outlook.com>
2025-06-26 15:20:38 -0700

    minor

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -21,11 +21,13 @@ return {
     providers = {
       claude = {
         endpoint = "https://api.anthropic.com",
-        model = "claude-sonnet-4-20250514",
+        -- model = "claude-sonnet-4-20250514",
+        model = "claude-3-5-haiku-20241022",
+
         timeout = 30000, -- Timeout in milliseconds
           extra_request_body = {
             temperature = 0.75,
-            max_tokens = 20480,
+            max_tokens = 8192,
           },
       },
     },
